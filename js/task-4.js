071b20b772fc6b7c9b81140f6dc3005683451418
alcasinter <151970978+alcasinter@users.noreply.github.com>
2024-03-03 21:20:24 +0100

    summary

---
+++ js/task-4.js
@@ -0,0 +1,27 @@
+function getShippingCost(country) {
+  var shippingCost;
+  switch (country) {
+    case "China":
+      shippingCost = 100;
+      break;
+    case "Chile":
+      shippingCost = 250;
+      break;
+    case "Australia":
+      shippingCost = 170;
+      break;
+    case "Jamaica":
+      shippingCost = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+  return "Shipping to " + country + " will cost " + shippingCost + " credits";
+}
+
+console.log(getShippingCost("Australia")); 
+console.log(getShippingCost("Germany")); 
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica")); 
+console.log(getShippingCost("Sweden")); 
